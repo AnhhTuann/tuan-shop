2b7ff8e9cdae303a3341069ea3baf834597f507c
AnhhTuann <tuannguyen25101999@gmail.com>
2024-10-16 00:22:15 +0700

    create product tab js

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,3 +35,19 @@ var swiperCategory = new Swiper(".category__container", {
 /*=============== SWIPER PRODUCTS ===============*/
 
 /*=============== PRODUCTS TABS ===============*/
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active-tab");
+    });
+    target.classList.add("active-tab");
+    tabs.forEach((tab) => {
+      tab.classList.remove("active-tab");
+    });
+    tab.classList.add("active-tab");
+  });
+});
