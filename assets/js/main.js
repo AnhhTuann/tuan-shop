6d7edf3a65fe79ccbedab8d6aea401473bed323d
AnhhTuann <tuannguyen25101999@gmail.com>
2024-10-16 19:04:32 +0700

    show product details

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,17 @@
 /* Validate if constant exists */
 
 /*=============== IMAGE GALLERY ===============*/
+function imgGallery() {
+  const mainImg = document.querySelector(".details__img"),
+    smallImg = document.querySelectorAll(".details__small-img");
+  smallImg.forEach((img) => {
+    img.addEventListener("click", function () {
+      mainImg.src = this.src;
+    });
+  });
+}
+
+imgGallery();
 
 /*=============== SWIPER CATEGORIES ===============*/
 var swiperCategory = new Swiper(".category__container", {
